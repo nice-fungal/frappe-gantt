afc23513efc7899fbf27a8d9a70afed24f2f28b9
hbrls <shuaizhexu@gmail.com>
2023-05-02 23:11:56 +0800

    [nice] build

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,23 @@
 import sass from 'rollup-plugin-sass';
 import { terser } from 'rollup-plugin-terser';
 
-const dev = {
-    input: 'src/index.js',
-    output: {
-        name: 'Gantt',
-        file: 'dist/frappe-gantt.js',
-        sourcemap: true,
-        format: 'iife',
-    },
-    plugins: [
-        sass({
-            output: true,
-        }),
-    ],
-};
-const prod = {
-    input: 'src/index.js',
-    output: {
-        name: 'Gantt',
-        file: 'dist/frappe-gantt.min.js',
-        sourcemap: true,
-        format: 'iife',
-    },
-    plugins: [
-        sass({
-            output: true,
-            options: {
-                outputStyle: 'compressed',
-            },
-        }),
-        terser(),
-    ],
-};
 
-export default [dev, prod];
+export default [
+  {
+    input: 'src/index.js',
+
+    plugins: [
+      sass({
+        output: true,
+        options: {
+          outputStyle: 'compressed',
+        },
+      }),
+      terser(),
+    ],
+
+    output: [
+      { file: 'dist/frappe-gantt-0.6.1-1.min.js', format: 'iife', name: 'FrappeGantt' },
+    ],
+  },
+];
